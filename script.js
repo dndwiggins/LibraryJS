9e8f49696851b845c09dfe66cad8c6a0fe52c37a
Dean <dndwiggins@gmail.com>
2024-07-12 17:14:40 -0400

    created DOM book

--- script.js
+++ script.js
@@ -8,17 +8,28 @@ function Book(title, author, pages, read) {
 
     this.info = function () {
         if (this.read == true) {
-            return this.title + "by" + this.author + "," + this.pages + "pages, has been read"
+            return this.title + " by " + this.author + ", " + this.pages + " pages, has been read"
         }
-        return this.title + "by" + this.author + "," + this.pages + "pages, not read yet"
+        return this.title + " by " + this.author + ", " + this.pages + " pages, not read yet"
     };
 
 }
 
 const book1 = new Book("To Kill a Mocking Bird", "IDK", 200, true);
 
-console.log(book1.info);
+
 
 function addBookToLibrary() {
     // do stuff here
+}
+
+
+function displayBooks() {
+    for (i = 0; i < myLibrary.length; i++) {
+
+
+
+
+    }
+
 }
